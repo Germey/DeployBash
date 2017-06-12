d959775bcdb3c331d0405bb3bd536a6caf9f097a
Germey <cqc@cuiqingcai.com>
2017-06-12 16:55:30 +0800

    Create docker-windows.ps1

--- docker-windows.ps1
+++ docker-windows.ps1
@@ -1,4 +1,3 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force
-Restart-Computer -Force
